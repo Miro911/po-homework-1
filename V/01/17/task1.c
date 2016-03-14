cbdb852e259fcc0fc0242ae7d77420e57894202e
Miro911 <mirooo250@abv.bg>
2016-03-14 16:49:48 +0200

    Update task1.c

--- V/01/17/task1.c
+++ V/01/17/task1.c
@@ -7,9 +7,10 @@ int main() {
 
 	char word[200];
 	
-	while( strlen(word) > 200 ){
+	do {
 		scanf("%s", word);
-	} 
+	} while( strlen(word) > 200 );
+	
 	printf("\n %ld", hash(word));
 
 	return 0;
@@ -20,7 +21,7 @@ long hash( char *word) {
 	int i = 0;
 	long hashs = 42;
 	
-	while(i <= strlen(word){
+	while(i <= strlen(word)){
 		hashs += word[i]*(i+1);
 		i++;
 	}
